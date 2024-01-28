340a3c6e781fbf40c389be8f04b4d382521ef823
Marc-Andre Rioux <marcandrioux@gmail.com>
2024-01-28 08:52:32 -0500

    put some stuff together

--- components/shooter.py
+++ components/shooter.py
@@ -1,7 +1,11 @@
 import magicbot
 import rev
+from magicbot import StateMachine, default_state, state, timed_state
 
 import constants
+from components import Intake
+
+from .lobra import LoBras
 
 
 class Shooter:
@@ -81,3 +85,33 @@ class ShooterFollower:
 
     def execute(self):
         pass
+
+
+class ShooterControl(StateMachine):
+    lobra: LoBras
+    shooter: Shooter
+    intake: Intake
+
+    def fire(self):
+        """This function is called from teleop or autonomous to cause the
+        shooter to fire"""
+        self.engage()
+
+    @state(first=True)
+    def prepare_to_fire(self):
+        """First state -- waits until shooter is ready before going to the
+        next action in the sequence"""
+        self.shooter.enable_shooter()
+
+        if self.shooter.is_ready():
+            self.next_state_now("firing")
+
+    @timed_state(duration=1, must_finish=True)
+    def firing(self):
+        """Fires the ball"""
+        self.intake.feed_shooter()
+
+    @default_state
+    def halt(self):
+        """Always called to stop the motor"""
+        self.shooter.disable_shooter()
